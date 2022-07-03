faf05ef4e53cc17b0dc3d96a280e24d27c6fdd05
prasadnads <prasadnads@gmail.com>
2022-07-04 01:11:14 +0530

    updated database and settings

--- resume_demo/settings.py
+++ resume_demo/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = 'resume_demo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.postgresql',
+             'NAME': 'resume_db',
+             'USER': 'postgres',
+             'PASSWORD':'Nandupr_92',
+             'HOST':'localhost'
+         }
+     }
+
+ 
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
